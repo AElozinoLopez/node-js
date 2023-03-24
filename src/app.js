35376e524af8f5e2f27c8d60945625958ad02865
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-03-24 21:32:41 +0100

    Experimenting with the event loop

--- src/app.js
+++ src/app.js
@@ -9,3 +9,31 @@
 // Without the object
 const printName = require('./node.js');
 printName('Asaije')
+
+
+// We have five console.log statements. Your goal is to have them log to the console in the order 
+// that they state without rearranging them. 
+// Your solution should include 1 console.log left as is with the other four using globals available
+//  within node.js to run the code at the appropriate time in the event loop. There are multiple ways to 
+//  accomplish this but you must use at least 3 different methods.
+
+
+const fs = require('fs');
+
+process.on('beforeExit', () => {
+    console.log('Print Fifth');
+}) 
+
+setImmediate(() => {
+    console.log('Print Third');
+}) 
+
+process.nextTick(() => {
+    console.log('Print Second');
+})
+
+console.log('Print First');
+
+setTimeout(() => {
+    console.log('Print Fourth');
+}, 5000) 
